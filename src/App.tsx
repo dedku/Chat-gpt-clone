07c6eaf9f3a9defbb8a623a06fc88548955f1d94
Patryk Szabat <szabat.patryk@gmail.com>
2023-04-20 16:42:10 +0200

    feat: Add HTML structure and basic CSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app">
+      <section className="sidebar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Patryk</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>PatrykGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input type="text" />
+            <div id="submit"></div>
+          </div>
+          <p className="info">
+          ChatGPT Mar 23 Version. Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts.
+          </p>
+        </div>
+      </section>
+    </div>
   )
 }
 
